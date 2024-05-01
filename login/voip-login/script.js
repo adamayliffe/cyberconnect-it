1a3a86c6b90a223ea2935b124c6c92a8d52919d8
adamayliffe <122642519+adamayliffe@users.noreply.github.com>
2024-05-01 15:22:35 +0100

    Add files via upload

---
+++ login/voip-login/script.js
@@ -0,0 +1,31 @@
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    // Get username and password
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+  
+    // Check credentials (You can replace this with your own authentication logic)
+    if (username === 'Admin' && password === 'Admin123') {
+      // Redirect to the page for user1
+      window.location.href = '/cyberconnect-it/voip-portal/support-center/';
+    } else if (username === 'aayliffe' && password === '!AnA160818!') {
+      // Redirect to the page for user2
+      window.location.href = '/cyberconnect-it/voip-portal/support-center/';
+    } else if (username === 'jehioghae' && password === 'Je2024!') {
+        // Redirect to the page for user2
+        window.location.href = '/cyberconnect-it/voip-portal/support-center/';
+      } else if (username === 'rmaxwell' && password === 'Rm2024!') {
+        // Redirect to the page for user2
+        window.location.href = '/cyberconnect-it/voip-portal/support-center/';
+      } else if (username === 'jr' && password === 'Jr2024!') {
+        // Redirect to the page for user2
+        window.location.href = '/cyberconnect-it/voip-portal/support-center/';
+      }
+
+     else {
+      // Display error message
+      document.getElementById('error-message').innerText = 'Invalid username or password';
+    }
+  });
+  
---
+++ status/script.js
@@ -0,0 +1,36 @@
+// Function to check website status
+function checkWebsite(url, monitorElement) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            var status = xhr.status;
+            var statusText = xhr.statusText;
+            var timestamp = new Date().toLocaleTimeString();
+
+            if (status >= 200 && status < 400) {
+                monitorElement.querySelector('.status').textContent = 'Online';
+                monitorElement.querySelector('.status').style.color = 'green';
+            } else {
+                monitorElement.querySelector('.status').textContent = 'Offline';
+                monitorElement.querySelector('.status').style.color = 'red';
+            }
+
+            monitorElement.querySelector('.timestamp').textContent = timestamp;
+        }
+    };
+    xhr.send();
+}
+
+// Check websites periodically
+setInterval(function() {
+    checkWebsite('https://adamayliffe.github.io/cyberconnect-it/', document.querySelectorAll('.monitor')[0]);
+    checkWebsite('https://adamayliffe.github.io/cyberconnect-it/login/', document.querySelectorAll('.monitor')[1]);
+    checkWebsite('https://adamayliffe.github.io/cyberconnect-it/admin-portal/admin-dashboard/', document.querySelectorAll('.monitor')[2]);
+    checkWebsite('https://adamayliffe.github.io/cyberconnect-it/login/client-login/', document.querySelectorAll('.monitor')[3]);
+
+    checkWebsite('https://adamayliffe.github.io/cyberconnect-it/admin-portal/service-engineering-dashboard/', document.querySelectorAll('.monitor')[4]);
+    checkWebsite('https://adamayliffe.github.io/cyberconnect-it/client-portal/support-center/', document.querySelectorAll('.monitor')[5]);
+    checkWebsite('https://adamayliffe.github.io/cyberconnect-it/voip-portal/support-center/', document.querySelectorAll('.monitor')[6]);
+    checkWebsite('https://adamayliffe.github.io/cyberconnect-it/login/voip-login/', document.querySelectorAll('.monitor')[7]);
+}, 1000); // Check every 1 seconds
